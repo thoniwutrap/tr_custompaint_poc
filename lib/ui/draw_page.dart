65e57e0abc27649f935864410e48a19c22040071
DESKTOP-IEM75PA\Tony <thoniwut.r.dev@gmail.com>
2019-06-08 10:49:18 +0700

    create custom widget custom paint and drawing model

--- lib/ui/draw_page.dart
+++ lib/ui/draw_page.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:tr_custompaint_poc/customwidget/drawing_widget.dart';
+import 'package:tr_custompaint_poc/model/drawing_points.dart';
 
 class DrawScreen extends StatefulWidget {
   @override
@@ -217,34 +219,5 @@ class _DrawState extends State<DrawScreen> {
   }
 }
 
-class DrawingPainter extends CustomPainter {
-  DrawingPainter({this.pointsList});
-  List<DrawingPoints> pointsList;
-  List<Offset> offsetPoints = List();
-  @override
-  void paint(Canvas canvas, Size size) {
-    for (int i = 0; i < pointsList.length - 1; i++) {
-      if (pointsList[i] != null && pointsList[i + 1] != null) {
-        canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
-            pointsList[i].paint);
-      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
-        offsetPoints.clear();
-        offsetPoints.add(pointsList[i].points);
-        offsetPoints.add(Offset(
-            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
-        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(DrawingPainter oldDelegate) => true;
-}
-
-class DrawingPoints {
-  Paint paint;
-  Offset points;
-  DrawingPoints({this.points, this.paint});
-}
 
 enum SelectedMode { StrokeWidth, Opacity, Color }
